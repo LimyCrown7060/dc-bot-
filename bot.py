47eb3827c405493b09d94b54745b0c8201046847
LimyCrown7060 <gumuskaan551@gmail.com>
2026-06-25 20:10:16 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,97 @@
+import os
+import random
+import discord
+import requests
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+@bot.command()
+async def sub(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left - right)
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+@bot.group()
+async def cool(ctx):
+    """Says if a user is cool.
+
+    In reality this just checks if a subcommand is being invoked.
+    """
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
+
+@bot.command()
+async def yardim(ctx):
+    """Botun komutlarını gösterir."""
+
+    await ctx.send("Merhaba! Komutlarım şunlar: \n$hello, $add, $sub, $repeat, $cool")
+
+@bot.command()
+async def mem(ctx):
+    files = os.listdir("images")
+    selected = random.choice(files)
+    with open(f'images/{selected}', 'rb') as f:
+        # Dönüştürülen Discord kütüphane dosyasını bu değişkende saklayalım!
+        picture = discord.File(f)
+   # Daha sonra bu dosyayı bir parametre olarak gönderebiliriz!
+    await ctx.send(file=picture)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    '''duck komutunu çağırdığımızda, program ordek_resmi_urlsi_al fonksiyonunu çağırır.'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+@bot.command()
+async def hayvan(ctx):
+    hayvanlar = [
+        "🐶 Köpek",
+        "🐱 Kedi",
+        "🐰 Tavşan",
+        "🦊 Tilki",
+        "🐼 Panda",
+        "🐧 Penguen"
+    ]
+
+    await ctx.send("Bugünün hayvanı: " + random.choice(hayvanlar))
+ 
+
+bot.run("MTUxMjc5Njk4NTY0MjcxMzE0MA.Gv3KMs.Mbg12b5mH9duFsCCq5XxzNnZ8WYB-1p6mTlyA8")
